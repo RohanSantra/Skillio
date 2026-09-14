18e6fb9c27a2c7f8e06be52b16d5dcb7caccaa77
Rohan <kajalsantra816@gmail.com>
2026-09-14 17:23:26 +0530

    commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -45,8 +45,9 @@ const setRefreshTokenCookie = (res, refreshToken) => {
     res.cookie("refreshToken", refreshToken, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
+        path: "/",
     });
 };
 
@@ -63,7 +64,8 @@ const clearRefreshTokenCookie = (res) => {
     res.clearCookie("refreshToken", {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
+        path: "/",
     });
 };
 
